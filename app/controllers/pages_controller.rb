d5dfc3415a7743cfba5f35cbe6cb6baaebd9266c
Howard Huang <howardhuang96@gmail.com>
2016-10-28 08:45:31 +0000

    log-in log

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,14 +13,5 @@ class PagesController < ApplicationController
         end
     end
     
-    def _navigation
-        @test = "WASSUP"
-        @logged_in = true
-        if current_user
-            @logged_in = true
-        else
-            @logged_in = false
-        end
-    end
             
 end
