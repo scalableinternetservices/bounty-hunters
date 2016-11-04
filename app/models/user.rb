f36d0ba134e00b2f46bce836bcbc5f385510636a
Siyu Chen <applepiefish@g.ucla.edu>
2016-11-04 12:58:00 -0700

    revert change

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
   #please work:http://stackoverflow.com/questions/9173412/rails-two-foreign-keys-on-one-model-both-refer-to-same-model       
   
   # when destroyed, destroy all posts
-  has_many :posts, class_name: 'Task', dependent: :destroy #, :foreign_key => :owner_id
+  has_many :posts, class_name: 'Task', dependent: :destroy, :foreign_key => :owner_id
   # when destroyed, nullify all claimsrai
   has_many :claims, class_name: 'Task', dependent: :nullify, :foreign_key => :claimer_id
 end
