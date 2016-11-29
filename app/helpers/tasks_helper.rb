d5384a151d099b5423fc60ee4d0b600f5d224036
Howard Huang <howardhuang96@gmail.com>
2016-11-29 03:53:14 +0000

    caching table

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -2,4 +2,9 @@ module TasksHelper
     def cache_key_for_task_row(task)
         "task-#{task.id}"
     end
+    
+    def cache_key_for_task_table
+        max_updated_at =
+        "task-table-#{Task.maximum(:updated_at)}"
+    end
 end
